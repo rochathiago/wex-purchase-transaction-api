629d0c3019412325e4c2e51d038d0dd8bdb80ed5
rochathiago <thiago_underground@hotmail.com>
2026-04-23 14:20:55 -0300

    Feat(feature_1): Currency Validator bugfix

--- API/Validators/CurrencyValidator.cs
+++ API/Validators/CurrencyValidator.cs
@@ -6,11 +6,9 @@ public class CurrencyValidator : AbstractValidator<string>
 {
     public CurrencyValidator()
     {
-        //RuleFor(x => x)
-        //    .NotEmpty()
-        //    .Length(3)
-        //    .Matches("^[A-Z]{3}$");
         RuleFor(x => x)
-            .NotEmpty();
+            .NotEmpty()
+            .Length(3)
+            .Matches("^[A-Z]{3}$");
     }
 }
